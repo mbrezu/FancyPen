df3261d60881098c33e379e9a25ee5df34dd5a88
Miron Brezuleanu <mbrezu@gmail.com>
2020-12-11 17:05:36 +0200

    Test the nuget packages.

---
+++ FancyPen.Json.NugetTest/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Text.Json;
+
+namespace FancyPen.Json.NugetTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var json = JsonDocument.Parse("{\"a\": 2}");
+
+            var result = PrettyPrinter.Print(json);
+
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ FancyPen.NugetTest/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text;
+
+namespace FancyPen.NugetTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var doc = Document.Concat(
+                "hello",
+                " ",
+                "world!"
+            );
+            var renderer = new Renderer();
+            var sb = new StringBuilder();
+
+            renderer.Render(doc, sb);
+
+            Console.WriteLine(sb);
+        }
+    }
+}
